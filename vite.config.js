e7acc725025b46d26a7ae2ed318e9a432387f44e
Hina Imran Butt <hinaimranbutt97@gmail.com>
2025-09-22 15:00:59 +0500

    fix design responsiveness

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/GSAP--Watchify/", // 🔴 Replace with your GitHub repo name
+    // base: "/", // 🔴 Replace with your GitHub repo name
+
 });
